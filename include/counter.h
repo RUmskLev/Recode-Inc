a10376da0284990533211eecc561f3e04bd74a57
DimaOsad23 <osadchiydima23022006@gmail.com>
2024-06-18 18:54:20 +0300

    update counter.h

--- include/counter.h
+++ include/counter.h
@@ -3,6 +3,15 @@
 #include <iostream>
 #include <fstream>
 
-
+/**
+*\file
+*Описание метода изменения счётчика
+*/
+/**
+*Закрытие воздушного пути
+*\param[in] losses количество поражений
+*\param[in] winnings количество побед
+*/
+void updateCounter(int losses, int winnnings);
 
 #endif
