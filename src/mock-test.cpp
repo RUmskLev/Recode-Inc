c91d8a65f94d313293e50a5ccee1c53a87dea900
DimaOsad23 <osadchiydima23022006@gmail.com>
2024-06-19 15:15:46 +0300

    Add mock

--- src/mock-test.cpp
+++ src/mock-test.cpp
@@ -1,13 +1,14 @@
-#include "world.h"
+#include "virus.h"
 #include <gmock/gmock.h>
 
-class MyMock: public Country{
+class MyMock: public Virus{
         public:
-                MOCK_METHOD(int, upInfected, (int inf));
+                MOCK_METHOD(int, upBlood, ());
 };
 
 TEST(Country_test, Test_get_noc)
 {
         MyMock mock(100, 1, 1);
-        EXPECT_CALL(mock, upInfected(100));
+        EXPECT_CALL(mock, upBlood());
+        mock.upBlood();
 }
